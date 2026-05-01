b6c67dbab558bfb2e0da9a35e538be9103d03712
Imran <imrankhanmohammed130@gmail.com>
2026-05-01 13:44:30 -0500

    learning terraformhc

--- main.tf
+++ main.tf
@@ -9,42 +9,71 @@ resource "aws_instance" "frontend" {
   }
 }
 
-resource "aws_instance" "postgresql" {
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+# resource "aws_instance" "postgresql" {
+#   ami           = "ami-0220d79f3f480ecf5"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+#
+#   tags = {
+#     Name = "postgresql"
+#   }
+# }
+#
+# resource "aws_instance" "auth-service" {
+#   ami           = "ami-0220d79f3f480ecf5"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+#
+#   tags = {
+#     Name  = "auth-service"
+#   }
+# }
+#
+# resource "aws_instance" "portfolio-service" {
+#   ami           = "ami-0220d79f3f480ecf5"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+#
+#   tags = {
+#     Name  = "portfolio-service"
+#   }
+# }
+#
+# resource "aws_instance" "analytics-service" {
+#   ami           = "ami-0220d79f3f480ecf5"
+#   instance_type = "t3.small"
+#   vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+#
+#   tags = {
+#     Name  = "analytics-service"
+#   }
+# }
 
-  tags = {
-    Name = "postgresql"
-  }
+resource "aws_route53_record" "frontend" {
+  zone_id   = "Z02346282FFZR4IVNTZCB"
+  name      = "frontend-dev"
+  type      =  "A"
+  tt1       =  30
+  records  = [aws_instance.frontend.private_ip]
+
 }
 
-resource "aws_instance" "auth-service" {
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
 
-  tags = {
-    Name  = "auth-service"
-  }
-}
 
-resource "aws_instance" "portfolio-service" {
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
 
-  tags = {
-    Name  = "portfolio-service"
-  }
-}
 
-resource "aws_instance" "analytics-service" {
-  ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.small"
-  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
 
-  tags = {
-    Name  = "analytics-service"
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
