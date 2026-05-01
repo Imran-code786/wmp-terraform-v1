73a910d96250a91f616933976b97cafa91837ef2
Imran <imrankhanmohammed130@gmail.com>
2026-05-01 13:32:01 -0500

    learning terraformhc

--- main.tf
+++ main.tf
@@ -8,3 +8,43 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+
+resource "aws_instance" "postgresql" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+
+  tags = {
+    Name = "postgresql"
+  }
+}
+
+resource "aws_instance" "auth-service" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+
+  tags = {
+    Name  = "auth-service"
+  }
+}
+
+resource "aws_instance" "portfolio-service" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+
+  tags = {
+    Name  = "portfolio-service"
+  }
+}
+
+resource "aws_instance" "analytics-service" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+
+  tags = {
+    Name  = "analytics-service"
+  }
+}
